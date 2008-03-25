125e5735aa873eb8f4070b20c5155910395c539d
alf <alf@c83b9c65-d100-4c72-8f92-b423b6639d83>
2008-03-25 12:50:37 +0000

    Initial import of RangeVote application
    git-svn-id: svn+ssh://alfmikula.is-a-geek.org/home/alf/svn_repos/apps/rangevote/trunk@36 c83b9c65-d100-4c72-8f92-b423b6639d83

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '97b306f5346dff7b378b7bade13c3b3b'
+end
